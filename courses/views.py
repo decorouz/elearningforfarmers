19aa97da02cf5c22f6303a88382b476a91d95c66
Biola <52200708+decorouz@users.noreply.github.com>
2022-06-21 11:05:28 +0100

    Add mixins and class-based views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.base import TemplateView
+
+from courses.models import Course
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+    '''Implement form that is used by views that use 
+    Django's ModelFormMixin mixin. View with form or model form.'''
+    
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ["series", "title", "slug", "overview"]
+    success_url = reverse_lazy("manage_course_list")
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = "courses/manage/course/form.html"
+
+
+class ManageCourseList(OwnerCourseMixin, ListView):
+    """Retrieve only courses created by the current user"""
+
+    template_name = "courses/manage/course/list.html"
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = "courses/manage/course/delete.html"
