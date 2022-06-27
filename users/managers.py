e5e90a9b9f49d9c007a96925c99a3f281e9940cf
Biola <52200708+decorouz@users.noreply.github.com>
2022-06-27 08:41:41 +0100

    Add Custom manager

--- users/managers.py
+++ users/managers.py
@@ -1,29 +1,31 @@
+from time import timezone
 from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
+    def _create(self, email, password, is_staff, is_superuser, **extrafields):
         if not email:
             raise ValueError("Users must have an email address")
 
+        now = timezone.now
+        email = self.normalize_email(email)
         user = self.model(
-            email=self.normalize_email(email),
+            email=email,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
+            **extrafields
         )
-
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(email, password=password)
-        user.is_admin = True
-        user.save(using=self._db)
+    def create_user(self, email, password, **extrafields):
+        print(**extrafields)
+        return self._create(email, password, False, False, **extrafields)
+
+    def create_superuser(self, email, password, **extrafields):
+        user = self._create(email, password, True, True, **extrafields)
         return user
