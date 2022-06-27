48b34df33e65b2c564997c7f1e0831e3364ca3bf
Biola <52200708+decorouz@users.noreply.github.com>
2022-06-27 08:40:39 +0100

    Create UserAdmin model

--- users/admin.py
+++ users/admin.py
@@ -2,36 +2,50 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .forms import UserAdminCreationForm, UserAdminChangeForm
-
 User = get_user_model()
 
 
 class UserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
-    form = UserAdminChangeForm
-    add_form = UserAdminCreationForm
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ["email", "is_admin"]
-    list_filter = ["is_admin"]
+    list_display = ["email", "is_staff"]
+    list_filter = ["is_staff"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Personal info", {"fields": ()}),
-        ("Permissions", {"fields": ("is_admin",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
-        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
     )
     search_fields = ("email",)
-    ordering = ("email",)
-    filter_horizontal = ()
+    ordering = ("date_joined",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
 
 
 # Register the new model
 admin.site.register(User, UserAdmin)
-admin.register()
