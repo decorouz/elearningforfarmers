d72728b5a22e84b58b82d38c09840f5a34930b70
Biola <52200708+decorouz@users.noreply.github.com>
2022-06-27 08:38:11 +0100

    Create CBV for courses

--- courses/views.py
+++ courses/views.py
@@ -3,6 +3,10 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 
 from courses.models import Course
 
@@ -18,18 +22,22 @@ class OwnerMixin(object):
 
 
 class OwnerEditMixin(object):
-    '''Implement form that is used by views that use 
-    Django's ModelFormMixin mixin. View with form or model form.'''
-    
+    """Implement form that is used by views that use
+    Django's ModelFormMixin mixin. View with form or model form."""
+
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(
+    OwnerMixin,
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+):
     model = Course
     fields = ["series", "title", "slug", "overview"]
-    success_url = reverse_lazy("manage_course_list")
+    success_url = reverse_lazy("courses:manage_course_list")
 
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
@@ -40,13 +48,18 @@ class ManageCourseList(OwnerCourseMixin, ListView):
     """Retrieve only courses created by the current user"""
 
     template_name = "courses/manage/course/list.html"
+    permission_required = "courses.view_course"
+    context_object_name = "my_courses"
 
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = "courses.add_course"
+
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = "courses.change_course"
+
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = "courses/manage/course/delete.html"
+    permission_required = "courses.delete_course"
