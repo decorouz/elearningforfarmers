7c8f2c78a33261fd7da76daf7bf7b53990495616
Biola <52200708+decorouz@users.noreply.github.com>
2022-06-27 08:42:14 +0100

    Create authentication urls

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
-
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+app_name = "users"
+
 urlpatterns = [
-    path("accounts/login", auth_views.LoginView.as_view(),name="login"),
-    path("accounts/logout", auth_views.LogoutView.as_view(),name="logout"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
