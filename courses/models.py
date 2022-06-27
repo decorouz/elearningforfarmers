d224a10f5c7002a62d31716bd3f9336bb9aa10ee
Biola <52200708+decorouz@users.noreply.github.com>
2022-06-27 08:35:39 +0100

    Add slugify and translation field

--- courses/models.py
+++ courses/models.py
@@ -25,6 +25,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from .fields import OrderField
 
+from django.utils.text import slugify
+
 
 User = get_user_model()
 
@@ -45,7 +47,7 @@ class Course(models.Model):
     owner = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
-        related_name="courses_created",
+        related_name="course_creator",
         verbose_name=_("Course creator"),
         null=True,
     )
@@ -53,13 +55,16 @@ class Course(models.Model):
         Series,
         on_delete=models.CASCADE,
         related_name="courses",
-        verbose_name=_("series"),
+        verbose_name=_("course series"),
     )
     title = models.CharField(max_length=200, verbose_name=_("Course title"))
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
     overview = models.TextField()
     hero_image = models.ImageField(
-        verbose_name=_("Course image"), upload_to="courses", null=True, blank=True
+        verbose_name=_("Course image"),
+        upload_to="courses",
+        null=True,
+        blank=True,
     )
     released_date = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -70,12 +75,16 @@ class Course(models.Model):
     class Meta:
         ordering = ["-released_date"]
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Course, self).save(*args, **kwargs)
+
 
 class Module(models.Model):
     course = models.ForeignKey(
         Course,
         related_name="modules",
-        verbose_name=_("Module"),
+        verbose_name=_("course module"),
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=200, verbose_name=_("Module title"))
@@ -144,10 +153,10 @@ class Question(models.Model):
         Course,
         related_name="questions",
         on_delete=models.CASCADE,
-        verbose_name=_("Test"),
+        verbose_name=_("questions"),
     )
     text = models.CharField(
-        max_length=1000, verbose_name=_("Questions"), null=True, blank=True
+        max_length=1000, verbose_name=_("text"), null=True, blank=True
     )
 
     def __str__(self) -> str:
@@ -156,9 +165,9 @@ class Question(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey(
-        Question, related_name="answers", on_delete=models.CASCADE
+        Question, related_name="correct_answer", on_delete=models.CASCADE
     )
-    text = models.CharField(max_length=1000, verbose_name=_("Answer to Question"))
+    text = models.CharField(max_length=1000, verbose_name=_("text"))
     correct_answer = models.BooleanField(blank=True)
 
     def __str__(self) -> str:
@@ -167,17 +176,24 @@ class Answer(models.Model):
 
 class UserResponse(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="userresponse"
+        User,
+        on_delete=models.CASCADE,
+        related_name="user",
+        verbose_name=_("users"),
     )
     question = models.ForeignKey(
-        Question, related_name="user_response_question", on_delete=models.CASCADE
+        Question,
+        related_name="question",
+        verbose_name=_("user response"),
+        on_delete=models.CASCADE,
     )
     response = models.ForeignKey(
         Answer,
         on_delete=models.DO_NOTHING,
         related_name="response_answer",
-        verbose_name=_("Your response"),
+        verbose_name=_("user answer"),
     )
 
     class Meta:
+        # ensures that a user provides an answer per question
         unique_together = ("question", "user")
