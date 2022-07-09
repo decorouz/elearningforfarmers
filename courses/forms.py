28d5016be1ece74814f7782c40252b7212ae7c7b
Biola <52200708+decorouz@users.noreply.github.com>
2022-07-09 14:08:04 +0100

    Add inlinemoduleform

---
+++ courses/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from .models import Course, Module
+
+# Since Course is divided into a variable number of
+# modules, it make sense to use formsets to manag them
+
+# Allow user to create multiple modules at once.
+
+ModuleInlineFormSet = inlineformset_factory(
+    Course, Module, fields=["title", "description"], extra=2, can_delete=True
+)
