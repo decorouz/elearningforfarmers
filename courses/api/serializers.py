829222241187e7b5273b5e6ef1f88f7f3b95186e
Biola <52200708+decorouz@users.noreply.github.com>
2022-07-16 13:14:15 +0100

    Create Series Serializer class

---
+++ courses/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from ..models import Series
+
+
+class SeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Series
+        fields = ("id" , "title", "slug")
