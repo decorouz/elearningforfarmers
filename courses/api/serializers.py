f435db2b26ee2cf97fe4f18b7983b6f5c65d4e9f
Biola <52200708+decorouz@users.noreply.github.com>
2022-07-16 15:21:14 +0100

    Create nested serializer

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
-from ..models import Series
+
+from ..models import Course, Series, Module
 
 
 class SeriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Series
-        fields = ("id" , "title", "slug")
+        fields = ("id", "title", "slug")
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ("order", "title", "description")
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "series",
+            "title",
+            "slug",
+            "overview",
+            "released_date",
+            "owner",
+            "modules",
+        )
+
